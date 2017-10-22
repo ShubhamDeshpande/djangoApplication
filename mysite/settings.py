c498db5bf7e37a060234f21f4d5aefa460a3051f
Shubham <you@example.com>
2017-10-22 14:11:17 +0530

    setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-#r@b25_cr55zfq*i7dn=%9!=-zq_of*9)32py#ymw1+w!orh2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
